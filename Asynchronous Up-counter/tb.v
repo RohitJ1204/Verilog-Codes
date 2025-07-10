4c86e35634b086aa9c4b5c0583c5fb62c8119d8e
Rohit J <126414437+RohitJ1204@users.noreply.github.com>
2025-07-10 12:13:51 +0530

    Create tb.v

---
+++ Asynchronous Up-counter/tb.v
@@ -0,0 +1,15 @@
+module asy_tb;
+wire [3:0]count;
+reg clk,rst;
+asy c1 (clk,rst,count);
+initial 
+begin
+clk <=1'b0;
+rst<=1'b0;
+#10
+rst<=1'b1;
+#200
+$stop;
+end
+always #5 clk<=~clk;
+endmodule 
